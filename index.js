7f60c10ffa7a664eb71aa40180900c91f61f31a3
Bek Zhuma <5906333+beknazar1@users.noreply.github.com>
2019-11-20 22:55:22 -0500

    use cors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
 
 morgan.token('post-content', function(req, res) {
   if (req.method === 'POST') 
@@ -11,6 +12,7 @@ const PORT = 3001;
 
 app.use(express.json());
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :post-content'));
+app.use(cors());
 
 let persons = [
   {
