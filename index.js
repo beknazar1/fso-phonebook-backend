1a42eae1290d9d5d76bcaf6b7a6b1f38071a3fd6
Bek Zhuma <5906333+beknazar1@users.noreply.github.com>
2019-11-25 14:48:07 -0500

    3.13 and 3.14

--- index.js
+++ index.js
@@ -1,113 +1,74 @@
-const express = require('express');
-const morgan = require('morgan');
-const cors = require('cors');
-
-morgan.token('post-content', function(req, res) {
-  if (req.method === 'POST') 
-    return JSON.stringify(req.body);
-})
-
+require("dotenv").config();
+const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 3001;
+const cors = require("cors");
+const Person = require("./models/person");
+const PORT = process.env.PORT;
 
-app.use(express.json());
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :post-content'));
-app.use(cors());
-app.use(express.static('build'));
-
-let persons = [
-  {
-    name: "Arto Hellas",
-    number: "040-123456",
-    id: 1
-  },
-  {
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-    id: 2
-  },
-  {
-    name: "Dan Abramov",
-    number: "12-43-234345",
-    id: 3
-  },
-  {
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-    id: 4
-  }
-];
-
-app.get("/api/persons", (req, res) => {
-  res.json(persons);
+// Logging configuration
+const morgan = require("morgan");
+morgan.token("post-content", function(req, res) {
+  if (req.method === "POST") return JSON.stringify(req.body);
 });
 
-app.get("/info", (req, res) => {
-  const now = new Date();
-  res.send(`Phonebook has info for ${persons.length} people\n${now}`);
+
+// Middleware
+app.use(express.json());
+app.use(
+  morgan(
+    ":method :url :status :res[content-length] - :response-time ms :post-content"
+  )
+);
+app.use(cors());
+app.use(express.static("build"));
+
+
+// API routing
+app.get("/api/persons", (req, res) => {
+  Person.find({}).then(persons => {
+    const arr = [];
+    persons.map(person => arr.push(person.toJSON()));
+    res.json(arr);
+  });
 });
 
 app.get("/api/persons/:id", (req, res) => {
-  const id = Number(req.params.id);
-  const person = persons.find(p => p.id === id);
-  if (!person) {
-    return res.status(404).json({
-      error: 'person does not exist'
-    })
-  }
-  res.json(person);
+  Person.findById(req.params.id).then(person => {
+    if (person === null) {
+      res.status(404).json({ error: "person does not exist" });
+    }
+    res.json(person.toJSON());
+  });
 });
 
 app.delete("/api/persons/:id", (req, res) => {
-  const id = Number(req.params.id);
-  const deletedPerson = persons.find(p => p.id === id);
-  if (!deletedPerson) {
-    return res.status(404). json({
-      error: 'person does not exist'
-    })
-  }
-  persons = persons.filter(p => p.id !== id);
-  res.json(deletedPerson);
+  Person.findByIdAndDelete(req.params.id).then(result => {
+    if (result === null) {
+      res.status(404).json({ error: "person does not exist" });
+    }
+    res.json(result.toJSON());
+  });
 });
 
 app.post("/api/persons", (req, res) => {
   const body = req.body;
 
   if (!body.name || !body.number) {
-    return res.status(400). json({
-      error: 'name and/or number is missing'
-    })
+    return res.status(400).json({
+      error: "name and/or number is missing"
+    });
   }
 
-  if (persons.find(person => person.name === body.name)) {
-    return res.status(403). json({
-      error: 'name already exists, but must be unique'
-    })
-  }
-  
-  let newPerson = {
-    name: body.name, 
-    number: body.number, 
-    id: generateId()
-  };
-  persons.push(newPerson);
+  let person = new Person({
+    name: body.name,
+    number: body.number
+  });
 
-  res.json(newPerson);
+  person.save().then(savedPerson => {
+    res.json(savedPerson.toJSON());
+  });
 });
-
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-
-const generateId = () => {
-  let randomId;
-  let retries = 0;
-  
-  do {
-    randomId = Math.floor(Math.random()*100000);
-    retries++;
-  } while (persons.find(person => person.id === randomId) && retries < 1000)
-  
-  return randomId;
-};
